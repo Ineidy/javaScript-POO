cda215c2c4a3b73e78b7eafeb11b4e935f33474d
Ineidy <160775201+Ineidy@users.noreply.github.com>
2024-07-09 08:53:21 -0500

    feat: :white_check_mark: Hedero Estudiante
    Crea una clase llamada Estudiante que herede de la clase Persona. Agrega una nueva propiedad llamada carrera y un método llamado estudiar que imprima en la consola un mensaj e indicando la carrera que está estudiando.

--- js/main.js
+++ js/main.js
@@ -9,18 +9,31 @@ class Persona {
     }
 }
 
+let persona1 = new Persona("Ineidy", 17, "Femenino")
+persona1.saludo();
+
+class Estudiante extends Persona{
+    constructor(nombre, edad, genero, carrera){
+        super( nombre, edad, genero)
+        this.carrera = carrera;
+    }
+    estudiar() {
+        console.log(`${this.nombre} estudia ${this.carrera}`);
+    }
+}
 document.querySelector('#contenidor').addEventListener('submit', function(event) {
     event.preventDefault();
     let nombre = document.querySelector('#name').value;
     let edad = document.querySelector('#edad').value;
     let genero = document.querySelector('#genero').value;
+    let carrera = document.querySelector('#carrera').value;
     let nueva = new Persona(nombre, edad, genero);
+    let nnn = new Estudiante(nombre, edad, genero, carrera);
     nueva.saludo();
-
+    nnn.estudiar();
+    
     document.querySelector('#name').value = '';
     document.querySelector('#edad').value = '';
     document.querySelector('#genero').value = '';
+    document.querySelector('#carrera').value = '';
 });
-
-let persona1 = new Persona("Ineidy", 17, "Femenino")
-persona1.saludo();
